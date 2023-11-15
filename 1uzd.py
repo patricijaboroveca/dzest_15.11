c26d5f78b3e10a16bfaeed4a3c03c148edb369e3
patricijaboroveca <patricija.boroveca@grobinasskola.lv>
2023-11-15 10:51:20 +0200

    Init commit

---
+++ 1uzd.py
@@ -0,0 +1,10 @@
+"""
+Izveidot programmu, kura prasa lietotājam ievadīt divus skaitlus un aprekina to summu izvadot terminali
+"""
+a=int(input('Ievadi 1 skaitli'))
+b=int(input('Ievadi 1 skaitli'))
+c=a+b
+print(f'a+b={c}')
+
+
+""" comitot """
